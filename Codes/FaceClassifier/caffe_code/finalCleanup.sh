b14839108a04929cc6ffe731f1a71d84cdf9daf1
oooolga <oooolga@guppy5.ais.sandbox>
2016-05-07 19:52:02 -0400

    commit changes

--- Codes/FaceClassifier/caffe_code/finalCleanup.sh
+++ Codes/FaceClassifier/caffe_code/finalCleanup.sh
@@ -1,11 +1,17 @@
-rm -rf ./train_lmdb
-rm -rf ./val_lmdb
+#rm -rf ./train_lmdb
+#rm -rf ./val_lmdb
 
-rm -f mean.binaryproto
+#rm -f mean.binaryproto
 
 SAVEDIR=/ais/gobi4/characters/Results/caffe/
-cp ./caffenet_train_iter_1500.caffemodel $SAVEDIR
-cp ./caffenet_train_iter_1500.solverstate $SAVEDIR
+for file in *.caffemodel;
+do
+	cp $file $SAVEDIR
+	rm -rf $file
+done 
 
-rm -rf ./caffenet_train_iter_1500.caffemodel
-rm -rf ./caffenet_train_iter_1500.solverstate
+for file in *.solverstate
+do
+	cp $file $SAVEDIR
+	rm -rf $file
+done
