441cfbc0ee194b850e23fb43709c0688c50625d0
oooolga <oooolga@guppy5.ais.sandbox>
2016-05-07 23:11:14 -0400

    commit changes

--- Codes/FaceClassifier/caffe_code/run_caffenet.sh
+++ Codes/FaceClassifier/caffe_code/run_caffenet.sh
@@ -4,6 +4,25 @@ echo "Output file name: "
 read FILENAME
 SAVEDIR=/ais/gobi4/characters/Results/caffe/
 DATE=`date +%Y%m%d_%H%M%S`
-echo Saving to $SAVEDIR$FILENAME$DATE.txt
-./train_caffenet.sh >> $SAVEDIR$FILENAME$DATE.txt
-./finalCleanup.sh
+mkdir $SAVEDIR$DATE
+cp solver.prototxt $SAVEDIR$DATE
+cp train_val.prototxt $SAVEDIR$DATE
+cp deploy.prototxt $SAVEDIR$DATE
+
+echo Saving to $SAVEDIR$DATE/$FILENAME.txt
+./train_caffenet.sh >> $SAVEDIR$DATE/$FILENAME.txt
+
+SAVEDIR=$SAVEDIR$DATE
+for file in *.caffemodel;
+do
+        cp $file $SAVEDIR
+        rm -rf $file
+done 
+
+for file in *.solverstate
+do
+        cp $file $SAVEDIR
+        rm -rf $file
+done
+
+#./finalCleanup.sh
