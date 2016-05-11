c084d408e3a7586d4c914d0bcf5ddbeae4fedf70
oooolga <oooolga@guppy5.ais.sandbox>
2016-05-10 22:09:51 -0400

    commit changes

--- Codes/FaceClassifier/caffe_code/train_caffenet.sh
+++ Codes/FaceClassifier/caffe_code/train_caffenet.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env sh
-
-/pkgs/caffe/bin/caffe.bin train \
-    --solver=./solver.prototxt
+#/pkgs/caffe/bin/caffe.bin device_query -gpu 2
+/pkgs/caffe/bin/caffe.bin train --solver=./solver.prototxt 
